40a4e3b2c8c895dbd5a2d08b037a16892a44ca5d
K S <skylerr091@gmail.com>
2024-11-10 14:48:40 +0100

    koniec

--- Lekcja 5/Source.cpp
+++ Lekcja 5/Source.cpp
@@ -8,21 +8,60 @@ using namespace std;
 
 bool czypierwsza(int p)
 {
-	if(p==1) return false;
-	if(p%2==0|| p%3==0) return false;
+	//if(p==1) return false;
+	//if(p%2==0|| p%3==0) return false;
+
+	/*for (int i = 2; i <= p; i++)
+	{
+		if (p % i == 0) 
+			return false;
+	}*/
+
+	return tab[p];
 }
 
-bool sito()
+void sito()
 {
 	tab[1] = true; // nie jest liczba pierwsza
 	// usuwanie wielokrotnosci 2
-	for (int i = 4; i <= MAX; i += 2) 
+	
+	/*for (int i = 4; i <= MAX; i += 2)
 		tab[i] = true;
 	for (int i = 3; i <= MAX; i += 3) 
-		tab[i] = true;
+		tab[i] = true;*/
+
+
+	for (int i = 2; i * i <= MAX; i++) // petla leci po wszystkich elementach
+	{
+		if (tab[i] == false)
+		{
+			// usuwanie wielokrtonosci i
+			for (int j = i * 2; j <= MAX; j += i)
+			{
+				tab[i] = true;
+			}
+		}
+	}
 }
 
 int main()
 {
+	int p,t;
+	
+	sito(); // odpalamy sito
+	
+	while (t--)
+	{
+		cin>>p;
+
+		if (czypierwsza(p)) // czypierwsza(p) == true
+		{
+			cout << "NIE" << endl;
+		}
+		else
+		{
+			cout << "TAK" << endl;
+		}
+	}
 	return 0;
 }
