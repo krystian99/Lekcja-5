37e0f45f391d1aabe8039aa3db33ee1290ec7190
K S <skylerr091@gmail.com>
2024-11-13 17:16:14 +0100

    dsfsf

--- Lekcja 5/Source.cpp
+++ Lekcja 5/Source.cpp
@@ -7,6 +7,8 @@ pair<char, int> tab[27];
 
 pair<char, int> zmienione[27];
 
+//char znaki[91];
+
 void ustaw()
 {
 	for (int i = 1; i <= 26; ++i)
@@ -38,16 +40,78 @@ void zamiana()
 	
 }
 
+string zaszyfruj(string slowo) // funkcja zwraca zaszyfrowane slowo
+{
+	for (int i = 0; i < slowo.size(); ++i) // petla leci po wszystkich znakach w slowie
+	{
+		// podpowiedz
+
+		// zmienione[1] - odwouluje sie Do litery A zmienionej na D
+		// zmienione[1].first = 'A'
+		// zmienione[1].second = 4
+		// slowo[i] -> znak char
+		// slowo[i] - 65 + 1 -> odowlanie sie do i-tego elementu tablicy zmienione
+		// zmienione['A' - 65 + 1].first -> zmienione[1].first -> 'A'
+		// zmienione['A' - 65 + 1].second -> zmienione[1].second -> 4
+		// tab['A' - 65 + 1].first -> tab[1].first -> 'A'
+		// tab['A' - 65 + 1].second -> tab[1].second -> 1
+
+		slowo[i] = tab[zmienione[slowo[i] - 65 + 1].second].first;
+	}
+
+	return slowo;
+}
+
+string zaszyfruj2(string slowo)
+{
+	for (int i = 0; i < slowo.size(); ++i) // petla leci po wszystkich znakach w slowie
+	{
+		if (slowo[i] > 'W')
+		{
+			if (slowo[i] == 'X')
+			{
+				slowo[i] = 'A';
+			}
+			if (slowo[i] == 'Y')
+			{
+				slowo[i] = 'B';
+			}
+			if (slowo[i] == 'Z')
+			{
+				slowo[i] = 'C';
+			}
+		}
+		else
+		{
+			slowo[i] = slowo[i] + 3;
+		}
+	}
+
+	return slowo;
+}
 
 int main()
 {
-	tab[1].first = 'A';
+	int s;
+	string slowo;
+	/*tab[1].first = 'A';
 	tab[1].second = 1;
 
 	tab[2].first = 'B';
-	tab[2].second = 2;
+	tab[2].second = 2;*/
 
 	ustaw();
+	zamiana();
+
+	cout<<"podaj ilosc slow";
+	cin>>s;
+
+	while(s--)
+	{
+		cin>>slowo;
+
+		cout << zaszyfruj2(slowo) << endl;
+	}
 	
 	return 0;
 }
