9fe8d2bbdeb437a027184fb2d196d1f7313f0107
K S <skylerr091@gmail.com>
2024-11-13 17:52:21 +0100

    sdfsfd

--- Lekcja 5/Source.cpp
+++ Lekcja 5/Source.cpp
@@ -2,8 +2,46 @@
 #include <string>
 
 using namespace std;
+bool sprawdzanie(string slowo)
+{
+	// kajak
+	// c = k
+	// c = i
+
+	//int j = slowo.size() - 1;
+	for (int i = 0; i < (slowo.size() + 1) / 2; i++)
+	{
+		for(int j = slowo.size() - 1;j>(slowo.size()+1)/2; j--)
+
+		//j--;
+	}
+		
+		
+
+
+	return false;
+}
+
+
+
 
 int main()
 {
+	int t;
+	cin>> t;
+	while(t--)
+	{
+		string s;
+		cin>> s;
+		if(sprawdzanie(s)== true)
+		{
+			cout << "TAK" << endl;
+		}
+		else
+		{
+			cout<<"NIE"<<endl;
+		}
+	}
 	return 0;
 }
+
